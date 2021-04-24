80fa4174bd994adecd9fa35aa3fe99937abfb6b9
onursokulluu <onur@templay.co>
2021-04-24 19:20:30 +0300

    initial

---
+++ Ecommerce.Application/Products/CreateProduct.cs
@@ -0,0 +1,22 @@
+﻿using Ecommerce.Database;
+using Ecommerce.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Application.Products
+{
+    public class CreateProducts
+    {
+        private ApplicationDbContext _context;
+        public CreateProducts(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Do(int id, string Name, string Description)
+        {
+            _context.Products.Add(new Product { Id = id, Name = Name, Description = Description });
+        }
+    }
+}
---
+++ Ecommerce.Database/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.Domain.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ecommerce.Database
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
